1938ff47094a27c52d04e363efc5a76aa2bbfbf1
Vathana-Bird <yeansovanvathanabird@gmail.com>
2024-01-16 10:22:01 +0700

    updated

--- Package.swift
+++ Package.swift
@@ -21,8 +21,7 @@ let package = Package(
                 .headerSearchPath("../HELib/helib_pack"),
                 .headerSearchPath("../HELib/helib_pack/include"),
                 .unsafeFlags(["--std=c++17"])
-            ],
-            
+            ]
         ),
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
