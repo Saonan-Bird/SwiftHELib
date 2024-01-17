7b7319057ad526503c9fbae736f0e4d6d4be0cfb
Saonan-Bird <thysaonan.bird@gmail.com>
2024-01-17 15:22:10 +0700

    Patch 7: Library Linker

--- Package.swift
+++ Package.swift
@@ -21,10 +21,10 @@ let package = Package(
                 .headerSearchPath("../HELib/helib_pack"),
                 .headerSearchPath("../HELib/helib_pack/include"),
                 .unsafeFlags(["--std=c++17"])
+            ],
+            linkerSettings: [
+                .unsafeFlags(["-LSources/HELib/helib_pack/lib", "-Wl,-rpath,Sources/HELib/helib_pack/lib", "-lgmp", "-lhelib", "-lntl"])
             ]
-//            linkerSettings: [
-//                .unsafeFlags(["-LSources/HELib/helib_pack/lib", "-lgmp", "-lhelib", "-lntl", "-Wl,-rpath,Sources/HELib/helib_pack/lib"])
-//            ]
         ),
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
